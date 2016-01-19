5ddf264f5fbf40571093dae42d9cb44be37691d8
Sauli Anto <sauli.anto@gmail.com>
2016-01-19 23:17:36 +0200

    Made dirty fix to update navbar when state is changing

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -1,25 +1,33 @@
 'use strict';
 
 angular.module('aklApp')
-    .controller('NavbarController', function ($scope, $location, $state, Auth, Principal, ENV, $http) {
+    .controller('NavbarController', function ($rootScope, $scope, $location, $state, Auth, Principal, ENV, $http) {
         $scope.isAuthenticated = Principal.isAuthenticated;
         $scope.$state = $state;
         $scope.inProduction = ENV === 'prod';
 
-        Principal.identity().then(function(account) {
-            $scope.account = account;
-            $scope.isAuthenticated = Principal.isAuthenticated;
+        var getAccount = function () {
+            Principal.identity().then(function(account) {
+                $scope.account = account;
+                $scope.isAuthenticated = Principal.isAuthenticated;
 
-            if ($scope.isAuthenticated) {
-                $http.get('api/steam/user/' + $scope.account.communityId).success(
-                    function (data) {
-                        $scope.steamUser = data;
-                });
-            }
-        });
+                if ($scope.isAuthenticated()) {
+                    $http.get('api/steam/user/' + $scope.account.communityId).success(
+                        function (data) {
+                            $scope.steamUser = data;
+                        });
+                }
+            });
+        };
+
+        getAccount();
 
         $scope.logout = function () {
             Auth.logout();
             $state.go('home');
         };
+
+        $rootScope.$on('$stateChangeStart',  function() {
+            getAccount();
+        });
     });
