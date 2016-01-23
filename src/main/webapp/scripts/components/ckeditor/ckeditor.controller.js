ab255ff8359e1f95e9521f5ca5d48ddf90b8ecfe
Sauli Anto <sauli.anto@gmail.com>
2016-01-23 16:46:55 +0200

    Added some plugins for ckeditor

--- src/main/webapp/scripts/components/ckeditor/ckeditor.controller.js
+++ src/main/webapp/scripts/components/ckeditor/ckeditor.controller.js
@@ -1,13 +1,19 @@
 'use strict';
 
 angular.module('aklApp')
-    .controller('CkeditorCtrl', function ($scope) {
+    .controller('CkeditorCtrl', function ($scope, $translate) {
 
+        // Load plugins from custom paths
+        CKEDITOR.plugins.addExternal('youtube',
+            '/bower_components/ckeditor-youtube-plugin/youtube/', 'plugin.js');
+        CKEDITOR.plugins.addExternal('autogrow',
+            '/scripts/components/ckeditor/plugins/autogrow/', 'plugin.js');
 
         // Editor options.
         $scope.options = {
             skin: 'bootstrapck,/bower_components/bootstrapck4-skin/skins/bootstrapck/',
-            language: 'fi',
+            language: $translate.use(),
+            extraPlugins: 'youtube,autogrow',
             allowedContent: true,
             entities: false
         };
