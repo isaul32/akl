591f3ac5e5ede1342f2997b521490df3aa5607d3
Sauli Anto <sauli.anto@gmail.com>
2016-01-23 19:56:47 +0200

    Made some calendar configs

--- src/main/webapp/scripts/app/schedule/schedule.controller.js
+++ src/main/webapp/scripts/app/schedule/schedule.controller.js
@@ -2,5 +2,32 @@
 
 angular.module('aklApp')
     .controller('ScheduleController', function ($scope) {
-        $scope.eventSources = [];
+
+        $scope.eventSources = [{
+            url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic"
+        }];
+
+        $scope.eventRender = function(event, element, view) {
+            /*element.attr({'tooltip': event.title,
+                'tooltip-append-to-body': true});
+            $compile(element)($scope);*/
+        };
+
+        $scope.uiConfig = {
+            calendar: {
+                editable: false,
+                //theme: true,
+                firstDay: 1,
+                header: {
+                    left: 'title',
+                    center: '',
+                    right: 'today prev,next'
+                },
+                eventClick: $scope.alertOnEventClick,
+                eventDrop: $scope.alertOnDrop,
+                eventResize: $scope.alertOnResize,
+                eventRender: $scope.eventRender
+            }
+        };
+
     });
