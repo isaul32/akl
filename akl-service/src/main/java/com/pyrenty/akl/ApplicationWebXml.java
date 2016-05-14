3fa866085258796a04c027042fa206dae7f11c2d
Sauli Anto <sauli.anto@gmail.com>
2016-05-15 02:49:57 +0300

    Restore Servlet Initializer

---
+++ akl-service/src/main/java/com/pyrenty/akl/ApplicationWebXml.java
@@ -0,0 +1,34 @@
+package com.pyrenty.akl;
+
+import com.pyrenty.akl.config.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * This is a helper Java class that provides an alternative to creating a web.xml.
+ * This will be invoked only when the application is deployed to a servlet container like Tomcat, Jboss etc.
+ */
+public class ApplicationWebXml extends SpringBootServletInitializer {
+
+    private final Logger log = LoggerFactory.getLogger(ApplicationWebXml.class);
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.profiles(addDefaultProfile())
+                .showBanner(false)
+                .sources(Application.class);
+    }
+
+    private String addDefaultProfile() {
+        String profile = System.getProperty("spring.profiles.active");
+        if (profile != null) {
+            log.info("Running with Spring profile(s) : {}", profile);
+            return profile;
+        }
+
+        log.warn("No Spring profile configured, running with default configuration");
+        return Constants.SPRING_PROFILE_DEVELOPMENT;
+    }
+}
