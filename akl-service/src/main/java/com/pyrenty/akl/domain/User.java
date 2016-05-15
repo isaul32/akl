817d2cc55509b068df2046c03c13115aace3710a
Sauli Anto <sauli.anto@gmail.com>
2016-05-15 21:45:08 +0300

    Change user table to akl_user (postgres conflict)

--- akl-service/src/main/java/com/pyrenty/akl/domain/User.java
+++ akl-service/src/main/java/com/pyrenty/akl/domain/User.java
@@ -19,7 +19,7 @@ import java.util.Set;
 import org.joda.time.DateTime;
 
 @Entity
-@Table(name = "\"user\"")
+@Table(name = "akl_user")
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class User extends AbstractAuditingEntity implements Serializable {
 
