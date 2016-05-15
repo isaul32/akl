2766d5b18970b091691fc29142f06ee9f539aef1
Juuso Tapaninen <juuso.tapaninen@gmail.com>
2016-05-15 22:52:42 +0300

    Refresh account details after verification

--- akl-app/src/main/javascript/scripts/app/account/activate/activate.controller.js
+++ akl-app/src/main/javascript/scripts/app/account/activate/activate.controller.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('aklApp')
-    .controller('ActivationController', function ($scope, $stateParams, Auth) {
+    .controller('ActivationController', function ($scope, $stateParams, Auth, Principal) {
         Auth.activateAccount({key: $stateParams.key}).then(function () {
             $scope.error = null;
             $scope.success = 'OK';
+            Principal.identity(true);
         }).catch(function () {
             $scope.success = null;
             $scope.error = 'ERROR';
