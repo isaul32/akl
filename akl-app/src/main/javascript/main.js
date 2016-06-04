6aec10dd8d6f29dea2ed086f6ce9eb80a1eb6d25
Sauli Anto <sauli.anto@gofore.com>
2016-06-05 00:34:40 +0300

    Add Google Analytics

--- akl-app/src/main/javascript/main.js
+++ akl-app/src/main/javascript/main.js
@@ -133,3 +133,12 @@ require('./scripts/app/entities/team/team.controller');
 require('./scripts/app/entities/team/team-detail.controller');
 require('./scripts/app/entities/team/team-dialog.controller');
 require('./scripts/app/entities/team/team-member-dialog.controller');
+
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-78802312-1', 'auto');
+ga('send', 'pageview');
